8b9a4df8307bacf721e9fa73fdca30ffde6950de
Vermasaiyam <vermasaiyam9@gmail.com>
2025-02-27 15:21:23 +0530

    day 3

--- Day3/p1.js
+++ Day3/p1.js
@@ -1,22 +1,22 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200, {'Content-Type':'text/html'});
-    if (req.url === '/home'){
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if (req.url === '/home') {
         res.end(`<h1>Home Page</h1>`);
     }
-    else if (req.url === '/about'){
+    else if (req.url === '/about') {
         res.end(`<h1>Aout Page</h1>`);
     }
-    else if (req.url === '/contact'){
+    else if (req.url === '/contact') {
         res.end(`<h1>Contact Page</h1>`);
     }
-    else{
+    else {
         res.end(`<h1>Page Not Found.</h1>`);
     }
 });
 
-server.listen(9000, (err)=>{
+server.listen(9000, (err) => {
     if (err)
         console.log("Error starting server", err);
     else
